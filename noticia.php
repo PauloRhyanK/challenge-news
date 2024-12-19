95aac4c0b63177bde8f7dd88671dd693df73c84d
Paulo Kuster <PauloRhyanK@github.com>
2024-12-19 14:18:35 -0300

    add new file `noticia.php`

---
+++ noticia.php
@@ -0,0 +1,31 @@
+<?php
+require 'db.php';
+
+$id = $_GET['id'] ?? null;
+if(!$id){
+    die("Noticias não encontrada!");
+}
+
+$stmt = $pdo->prepare("SELECT * FROM noticias WHERE id = ?");
+$stmt->execute([$id]);
+$noticia = $stmt->fetch();
+
+if (!$noticia){
+    die("Noticia não encontrada!");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title><?= htmlspecialchars($noticia['titulo']) ?></title>
+</head>
+<body>
+    <h1><?= htmlspecialchars($noticia['titulo']) ?></h1>
+    <p><?= nl2br(htmlspecialchars($noticia['descricao'])) ?></p>
+    <p><a href="<?= htmlspecialchars($noticia['link']) ?>" target="_blank">Leia mais</a></p>
+    <p>Data de publicação: <?= $noticia['data_publicacao'] ?></p>
+    <a href="index.php">Voltar</a>
+</body>
+</html>
